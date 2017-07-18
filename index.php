7aebeb5d11a3cada607542e0634a43c14fe1f90b
esteban1234 <tva_libra@hotmail.com>
2017-07-18 15:01:15 -0500

    inicio de pagina

---
+++ index.php
@@ -0,0 +1,89 @@
+
+<?php include 'vistas/encabezado.php' ?>
+<body>
+<header>
+  <section class="banner" id="index">
+    <div class="contenmenulogo">
+      <div class="menu pull-left">
+          <ul>
+            <a class="marca select" href="#index"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;INICIO</a>
+            <a class="marca" href="#nosotros">NOSOTROS</a>
+            <a class="marca" href="#servicios">SERVICIOS</a>
+            <a class="marca" href="#contacto">CONTACTO</a>
+          </ul>
+      </div>
+      <figure class="logo pull-right">
+        <img src="img/lomram.png" class="img-responsive" alt="">
+      </figure>
+    </div>
+
+    <p>BOTANAS Y ADEREZOS QUE SE ANTOJAN</p>
+    <div class="boton" >
+      <a href="#contacto"><button type="button" class="btn btn-success btn-lg">Contáctanos</button></a>
+    </div>
+  </section>
+  <br><br>
+
+  <section class="nosotros" id="nosotros">
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 style="color: #4D2448;">La empresa</h1>
+            <h4 style="color: #4D2448;">GRUPO MANUFACTURERO LOMRAM S.A. DE C.V.</h4> <br>
+            <p style="font-family: 'Montserrat', sans-serif;">Somos una empresa orgullosamente Mexicana, con años en el mercado de las botanas. <br><br>
+
+            Nuestros productos son elaborados tradicionalmente con materias primas de primera calidad y dentro de los estándares modernos de higiene; así garantizando el mejor sabor de nuestras botanas, lo que los ha llevado a ser la botana preferida del mercado. <br><br>
+
+            Hemos acompañado festejos, eventos familiares, reuniones de trabajo, y hemos estado cerca de ti disfrutando de todos esos momentos especiales que le dan sabor a la vida.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="img/totopos.jpg" class="img-responsive img-rounded">
+          </div>
+        </div>
+      </div>
+    </div>
+  <br><br>
+
+  <div class="container" id="features">
+  <div class="row">
+    <div class="col-md-4 feature">
+          <i class="fa fa-check" aria-hidden="true"></i>
+            <h3>MISION</h3>
+            <div class="title_border"></div>
+            <p>Proveer con responsabilidad y excelencia, botanas y frituras, como papas fritas, chicharrones de harina y de cerdo, tostadas, cacahuates, semillas fritas, palomitas de maíz, etc. utilizando la materia prima e insumos de calidad y así garantizar el exquisito sabor de nuestros productos a nuestros consumidores.</p>
+    </div>
+        <div class="col-md-4 feature">
+            <i class="fa fa-line-chart" aria-hidden="true"></i>
+            <h3>VISION</h3>
+            <div class="title_border"></div>
+            <p>Ser una empresa líder en los mercados de productos alimenticios especialmente en botanas, cumpliendo con los estándares de higiene, calidad y servicio requeridos por nuestros clientes, desarrollando tecnología que asegure la mejora continua y el crecimiento en nuevos productos.</p>
+    </div>
+        <div class="col-md-4 feature">
+            <i class="fa fa-users" aria-hidden="true"></i>
+            <h3>VALORES</h3>
+            <div class="title_border"></div>
+            <p>
+            * Trabajo en equipo <br>
+            * Responsabilidad <br>
+            * Compromiso <br>
+            * Honestidad <br>
+            * Respeto <br>
+            * Calidad
+            </p>
+    </div>
+  </div>
+</div>
+</section> 
+<span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
+<?php include 'vistas/pie.php' ?>
+
+<script src="js/jquery.js"></script>
+<script src="js/menu.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/sendCORREO.js"></script>
+<script src="js/botonarriba.js"></script>
+<script src="https://pingendo.com/assets/scripts/smooth-scroll.js"></script>
+
+</body>
+</html>
---
+++ vistas/encabezado2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	
+	<link rel="icon" type="image/png" href="../img/favicon.png" />
+	<title>LOMRAM</title>
+	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+  	<META NAME="Title" content="Lomram">
+  	<META NAME="Author" CONTENT="[http://lomram.com/]"/>
+  	<META name="Description" content="Somos una organización innovadora, confiable y líder, con presencia a otros mercados, operando eficientemente los productos propios y distribución de productos complementarios para lograr rentabilidad y crecimiento sostenido.."/>
+     <META NAME="Keywords" CONTENT="Botanas, Charritos, Churritos, Fiesta."/>
+  	<link rel="stylesheet" href="../css/bootstrap.css">
+  	<link rel="stylesheet" href="../css/iconos.min.css">
+	<link rel="stylesheet" href="../css/estilos.css">
+	<link rel="stylesheet" href="../css/responsivo.css">
+	<script src="../js/general.js"></script>
+</head>
