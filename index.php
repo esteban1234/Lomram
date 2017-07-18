1dc43519711b0e8ae75ab7b8ba5b50c855a7a71a
esteban1234 <tva_libra@hotmail.com>
2017-07-18 17:39:44 -0500

    pagina lista

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
           <ul>
             <a class="marca select" href="#index"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;INICIO</a>
             <a class="marca" href="#nosotros">NOSOTROS</a>
-            <a class="marca" href="#servicios">SERVICIOS</a>
+            <a class="marca" href="#productos">PRODUCTOS</a>
             <a class="marca" href="#contacto">CONTACTO</a>
           </ul>
       </div>
@@ -74,7 +74,151 @@
     </div>
   </div>
 </div>
-</section> 
+</section>
+
+<section class="productos" id="productos">
+<div class="section">
+      <div class="container">
+      <h1 style="color: #4D2448; text-align: center;font-family: 'Montserrat', sans-serif;">PRODUCTOS</h1> <br><br>
+        <div class="row">
+          <div class="col-md-3">
+            <img class="imagen" src="img/charrito.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">CHARRITOS</h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+          <div class="col-md-3">
+            <img class="imagen" src="img/tostadas.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">TOSTADAS </h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+          <div class="col-md-3">
+            <img class="imagen" src="img/totopos.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">TOTOPOS</h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+          <div class="col-md-3">
+            <img class="imagen" src="img/churro.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">Churro natural</h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+        </div>
+        
+        <br><br>
+        <div class="row">
+          <div class="col-md-3">
+            <img class="imagen" src="img/chicha.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">Chicharrón enchilado</h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+
+          <div class="col-md-3">
+            <img class="imagen" src="img/caca2.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">Cacahuates salados</h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+          
+          <div class="col-md-3">
+            <img class="imagen" src="img/caca.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">Cacahuates enchilado</h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+          <div class="col-md-3">
+            <img class="imagen" src="img/aderezo.jpg"
+            class="img-responsive img-rounded">
+            <h2 class="text-center" style="color: #1177BC;">Aderezo César Vegano</h2>
+            <!-- <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p> -->
+          </div>
+        </div>
+
+      </div>
+    </div>
+    </section>
+
+
+      <section id="contacto" style="">
+            <div class="container">
+                <div class="row">
+                    <div class="about_our_company" style="margin-bottom: 20px;">
+                        <h1 style="color:#fff;">Contáctanos</h1>
+                        <div class="titleline-icon"></div>
+                        <p style="color:#fff;">Escribenos tus comentario, dudas o preguntas, estamos para servirte.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-8">
+                        <form name="sentMessage" id="contactForm" novalidate="">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Escribe tu Nombre Completo" id="name" required="" data-validation-required-message="Escribe tu Nombre Completo">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Escribe tu Correo Completo" id="email" required="" data-validation-required-message="Escribe tu Correo Completo">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="tel" class="form-control" placeholder="Escribe tu Teléfono Completo" id="phone" required="" data-validation-required-message="Escribe tu Teléfono Completo">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <textarea class="form-control" placeholder="Escribe tu Mensaje" id="message" required="" data-validation-required-message="Escribe tu Mensaje"></textarea>
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                                <div class="clearfix"></div>
+                                <div class="col-lg-12 text-center">
+                                    <div id="success"></div>
+                                    <button type="submit" class="btn btn-success btn-xl get">ENVIAR</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-4">
+                        <p style="color:#fff;">
+                            <strong><i class="fa fa-map-marker"></i> Dirección</strong><br>
+                            CALLE 94 # 571  ENTRE 11-A-2 Y 81-F COL. RESIDENCIAL PENSIONES C.P. 97127 MÉRIDA, YUCATÁN.
+                        </p>
+                        <p style="color:#fff;"><strong><i class="fa fa-phone"></i> Teléfono</strong><br>
+                            Pendiente</p>
+                        <p style="color:#fff;">
+                            <strong><i class="fa fa-envelope"></i>  Correo</strong><br>
+                            info@lomram.com</p>
+
+                        <p style="color:#fff;">
+                            <strong><i class="fa fa-clock-o"></i>  Horario</strong><br>
+                            Lunes a Viernes de 9am a 6pm</p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
 <?php include 'vistas/pie.php' ?>
 
