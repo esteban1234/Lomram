70e369a76db33da63da22eb710220241fb26ab2d
esteban1234 <tva_libra@hotmail.com>
2017-07-20 11:07:12 -0500

    cambio de correo

--- vistas/envia.php
+++ vistas/envia.php
@@ -22,8 +22,7 @@ $mail->From = $correo;
 $mail->FromName = $nombre;
 
 //To address and name
-// $mail->addAddress("info@lomram.com");
-$mail->addAddress("ballina.santiago@gmail.com");
+$mail->addAddress("info@lomram.com");
 
 
 $mail->isHTML(true);
