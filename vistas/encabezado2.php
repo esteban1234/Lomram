f2d085d690a88b41d6550124f7644cf0c3c494ad
esteban1234 <tva_libra@hotmail.com>
2017-07-21 12:24:38 -0500

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="y3y9fU6qRzNbg9Zr5IJHxti0pP2WE77cjROkzEiVcl0" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>LOMRAM</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
