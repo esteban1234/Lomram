3b33a31eceaa6a0f3f3c06831844daa731343c73
champiiy <albert1047@hotmail.com>
2017-07-31 18:40:26 -0500

    ERROR 01

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>LOMRAM</title>
-	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   	<META NAME="Title" content="Lomram">
   	<META NAME="Author" CONTENT="[http://lomram.com/]"/>
   	<META name="Description" content="Somos una organización innovadora, confiable y líder, con presencia a otros mercados, operando eficientemente los productos propios y distribución de productos complementarios para lograr rentabilidad y crecimiento sostenido.."/>
